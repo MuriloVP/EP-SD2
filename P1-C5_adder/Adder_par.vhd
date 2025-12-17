01b31dea1543e12de91997be52efcb312a81c2b6
daniel-trierweiler <daniel.trierweiler@usp.br>
2025-12-16 21:11:33 -0300

    Rayan > Yamal

--- P1-C5_adder/Adder_par.vhd
+++ P1-C5_adder/Adder_par.vhd
@@ -1,3 +1,15 @@
+------------------------------------------------------
+--- PROJETO POLILEGV8 - TURMA 1 - GRUPO 1 - T1G01 ----
+------------------------------------------------------
+--- INTEGRANTES --------------------------------------
+------------------------------------------------------
+--- DANIEL TRIERWEILER LEAL CORRÊA - 15446990 --------
+--- GUILHERME GADIOLI MARCELINO MARTINS - 15507545 ---
+--- JOÃO VICTOR DE ALCÂNTARA FREDO - 15638419 --------
+--- MAURÍLIO MIRANDA LAGO - 15513111 -----------------
+--- MURILO VITOR PIERETTI - 15481030 -----------------
+------------------------------------------------------
+
 library ieee;
 use ieee.numeric_bit.all;
 
